aaae8e1e3accb4f60cec75d8ced780062eaeb2b8
Maria Eduarda Leiol <68747904+Leiol@users.noreply.github.com>
2021-08-08 21:09:43 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pokedexx/views/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'pokedex',
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ lib/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/painting.dart';
+
+Color redTheme = Color(0XFFE3350D);
+Color orangeTheme = Color(0XFFF17F2E);
+Color darkYellowTheme = Color(0XFFA48C22);
+Color blueTheme = Color(0XFF688FF3);
